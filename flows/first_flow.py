4a68c5817419f4d1eeddcf85ed8295258da7c514
Dendi Handian <contact.dendi@yahoo.com>
2023-02-25 02:10:05 +0700

    verbose first_flow example

--- flows/first_flow.py
+++ flows/first_flow.py
@@ -1,12 +1,13 @@
 from prefect import task, flow
+from datetime import datetime
 
 @task
 def first_task():
-    print('doing first_task')
+    print(f"first_task executed at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 @flow
 def first_flow():
-    print('first_flow running')
+    print(f"first_flow running at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     first_task()
 
 if __name__ == '__main__':
