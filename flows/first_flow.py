4638b76d3087d3a8da75834d8684723522484007
Dendi Handian <contact.dendi@yahoo.com>
2023-02-25 02:39:33 +0700

    add sleep to first_task

--- flows/first_flow.py
+++ flows/first_flow.py
@@ -1,9 +1,13 @@
 from prefect import task, flow
 from datetime import datetime
+from random import randint
+from time import sleep
 
 @task
 def first_task():
-    print(f"first_task executed at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print(f"first_task started at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    sleep(randint(1, 10))
+    print(f"first_task finished at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 @flow
 def first_flow():
