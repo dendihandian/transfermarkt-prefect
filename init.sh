b8273220864a15995720ca82b3a63c465bbd50df
Dendi Handian <contact.dendi@yahoo.com>
2023-02-28 01:25:52 +0700

    prefect work-pool set-concurrency-limit default-agent-pool 1

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+prefect work-pool set-concurrency-limit default-agent-pool 1
 prefect work-queue set-concurrency-limit default 1
+
 prefect deployment apply deployments/transfermarkt_incremental__everyFiveMinute.yaml
 prefect deployment set-schedule transfermarkt_incremental/everyFiveMinute --cron '*/5 * * * *'
 
