8a3f83d3d40dd3b6f72611a3ac1eeb133f22a193
Dendi Handian <contact.dendi@yahoo.com>
2023-03-04 05:27:29 +0700

    fix and enhance docker-compose

--- init.sh
+++ init.sh
@@ -5,5 +5,5 @@ prefect work-queue set-concurrency-limit default 1
 
 prefect deployment apply deployments/transfermarkt_incremental__everyFiveMinute.yaml
 prefect deployment set-schedule transfermarkt_incremental/everyFiveMinute --cron '*/5 * * * *'
-
-prefect agent start -q default
+prefect deployment apply deployments/transfermarkt_incremental_page__everyFiveMinute.yaml
+prefect deployment set-schedule transfermarkt_incremental_page/everyFiveMinute --cron '*/5 * * * *'
