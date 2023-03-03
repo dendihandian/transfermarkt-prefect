aabca2ad0c4791397cd22746b28c3077c6f19af6
Dendi Handian <contact.dendi@yahoo.com>
2023-03-04 06:22:36 +0700

    prefect agent start only work in init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,9 @@ prefect work-pool set-concurrency-limit default-agent-pool 1
 prefect work-queue set-concurrency-limit default 1
 
 prefect deployment apply deployments/transfermarkt_incremental__everyFiveMinute.yaml
-prefect deployment set-schedule transfermarkt_incremental/everyFiveMinute --cron '*/5 * * * *'
 prefect deployment apply deployments/transfermarkt_incremental_page__everyFiveMinute.yaml
-prefect deployment set-schedule transfermarkt_incremental_page/everyFiveMinute --cron '*/5 * * * *'
+
+prefect deployment set-schedule transfermarkt-incremental/everyFiveMinute --cron '*/5 * * * *'
+prefect deployment set-schedule transfermarkt-incremental-page/everyFiveMinute --cron '*/5 * * * *'
+
+prefect agent start -q default
