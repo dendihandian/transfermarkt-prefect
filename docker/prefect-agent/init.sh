989daa1e93310ac0febb679529333fac88be94b6
Dendi Handian <contact.dendi@yahoo.com>
2023-03-11 06:17:44 +0700

    set-concurrency-limit default-agent-pool

--- docker/prefect-agent/init.sh
+++ docker/prefect-agent/init.sh
@@ -3,6 +3,8 @@
 
 prefect config set PREFECT_API_DATABASE_CONNECTION_URL="postgresql+asyncpg://prefect:prefect@postgres:5432/prefect_db"
 
+prefect work-pool set-concurrency-limit default-agent-pool 1
+
 prefect deployment apply src/deployments/transfermarkt_incremental__everyMinute.yaml
 prefect deployment apply src/deployments/transfermarkt_incremental_page__everyFiveMinute.yaml
 
