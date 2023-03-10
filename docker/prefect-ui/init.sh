fb1ee89e6310c3b08969204ea1c4e8f22a78c136
Dendi Handian <contact.dendi@yahoo.com>
2023-03-11 04:58:31 +0700

    if you're using windows, make sure this init.sh file encoded to LF instead of CRLF

--- docker/prefect-ui/init.sh
+++ docker/prefect-ui/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# if you're using windows, make sure this init.sh file encoded to LF instead of CRLF
+
 prefect config set PREFECT_API_DATABASE_CONNECTION_URL="postgresql+asyncpg://prefect:prefect@postgres:5432/prefect_db"
 
 prefect server start --host 0.0.0.0 --port 4200
